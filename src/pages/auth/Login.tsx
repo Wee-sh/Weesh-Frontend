aebca4ce7842541e437f75006f862c07906143a0
김소희 <ttohee1275@gmail.com>
2025-11-14 00:18:35 -0800

    feat: 트리 추가

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,16 +1,22 @@
 import weesh_letter from "../../assets/decoration/weesh-letter.png";
 import KakaoOauthButton from "../../components/auth/KakaoOauthButton";
 import insta_icon from "../../assets/Icon/insta_icon.svg";
-
+import tree from "../../assets/decoration/image-removebg-preview-51 1.png";
+import tree_halo from "../../assets/decoration/Vector 3.png";
 const Login = () => {
   return (
     <div className="relative w-screen min-h-screen bg-cover bg-center bg-no-repeat bg-[url('/src/assets/background/weesh-login-bg.png')]">
       <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-black/10" />
       <div className="relative w-full h-screen flex flex-col items-center">
         <img src={weesh_letter} className="mt-[138px]" />
-        <span className="w-10 h-[408px] bg-slate-300 break-all">
-          트리? 입니다 ㅎㅎ ..
-        </span>
+        <div>
+          <img
+            src={tree_halo}
+            className="absolute [filter:drop-shadow(0_0_29.7px_#606018)_blur(3.85px)]"
+          />
+          <img src={tree} className="relative h-[408px]" />
+        </div>
+
         <div className="flex flex-col items-center">
           <h1 className="text-2xl text-white mb-[38px]">
             Wee-sh You a Merry Christmas
