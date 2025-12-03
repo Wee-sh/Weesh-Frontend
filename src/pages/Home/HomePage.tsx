084eade1060bc36fde2b077a27e3fa6bba0c90dd
김소희 <ttohee1275@gmail.com>
2025-12-03 23:00:19 +0900

    feat: 홈 화면 퍼블리싱 완료

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,13 +1,33 @@
 import { useState } from "react";
 import setting_cookie from "../../assets/Icon/setting-cookie.svg";
 import post from "../../assets/decoration/post.svg";
+import tree_0 from "../../assets/decoration/trees/tree_0.png";
+import tree_1 from "../../assets/decoration/trees/tree_1.png";
+import tree_2 from "../../assets/decoration/trees/tree_2.png";
+import tree_3 from "../../assets/decoration/trees/tree_3.png";
+import tree_4 from "../../assets/decoration/trees/tree_4.png";
+import tree_5 from "../../assets/decoration/trees/tree_5.png";
+import star from "../../assets/decoration/star.svg";
+
+const trees = [
+  { id: 0, url: tree_0 },
+  { id: 1, url: tree_1 },
+  { id: 2, url: tree_2 },
+  { id: 3, url: tree_3 },
+  { id: 4, url: tree_4 },
+  { id: 5, url: tree_5 },
+];
 
 const HomePage = () => {
   const [gift, setGift] = useState(0);
 
   const handleGift = () => {
-    setGift(gift + 1);
+    if (gift < 5) {
+      setGift(gift + 1);
+    }
   };
+
+  const currentTree = trees.find((tree) => tree.id === gift);
 
   return (
     <div className="w-screen min-h-screen relative bg-cover bg-center bg-no-repeat bg-[url('src/assets/background/weesh-home-bg.png')]">
@@ -28,9 +48,27 @@ const HomePage = () => {
       <div className="w-full h-[25%] absolute bottom-0">
         <div
           onClick={handleGift}
-          className="w-full h-full  flex justify-center relative bg-cover bg-center bg-no-repeat bg-[url('src/assets/decoration/snow.svg')]"
+          className="w-full h-full flex justify-center relative bg-cover bg-center bg-no-repeat bg-[url('src/assets/decoration/snow.svg')]"
         >
-          <img src="" className="absolute bottom-0" />
+          {currentTree && (
+            <div className="absolute bottom-[130px] w-[393px] flex justify-center">
+              <div
+                className="absolute top-5 right-[144px] w-[116px] h-[126px] bg-cover bg-center bg-no-repeat flex items-center justify-center"
+                style={{ backgroundImage: `url(${star})` }}
+              >
+                <span
+                  style={{
+                    textShadow:
+                      "-2px 0px #E0B81A, 0px 2px #E0B81A, 2px 0px #E0B81A, 0px -2px #E0B81A",
+                  }}
+                  className="text-xl text-white"
+                >
+                  {100}
+                </span>
+              </div>
+              <img src={currentTree.url} className="w-full" />
+            </div>
+          )}
           <img src={post} className="w-16 h-14 absolute right-4 bottom-14" />
         </div>
       </div>
